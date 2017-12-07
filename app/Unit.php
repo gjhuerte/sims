fbcdfa6f56a0d20e788997ea55af4b42263a988a
Gabriel Jay Huerte <gjhuerte@gmail.com>
2017-12-07 16:35:57 +0800

    Updated Validation for Unit
    Added Unique attribute as Validation

--- app/Unit.php
+++ app/Unit.php
@@ -11,9 +11,9 @@ class Unit extends Model{
 	protected $fillable = ['name','description'];
 	protected $primaryKey = 'id';
 	public static $rules = array(
-		'Name' => 'required',
+		'Name' => 'required|unique:units,name',
 		'Description' => '',
-		'Abbreviation' => 'required'
+		'Abbreviation' => 'required|unique:units,abbreviation'
 	);
 
 	public static $updateRules = array(
