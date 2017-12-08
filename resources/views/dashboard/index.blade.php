c4ac245df126f1f2a9c1b584093da5a7d56fbf2f
Gabriel Jay Huerte <gjhuerte@gmail.com>
2017-12-08 18:24:54 +0800

    Dashboard Update

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -22,8 +22,8 @@
         <span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>
 
         <div class="info-box-content">
-          <span class="info-box-text">CPU Traffic</span>
-          <span class="info-box-number">90<small>%</small></span>
+          <span class="info-box-text">Purchase Order</span>
+          <span class="info-box-number">{{ isset($purchaseorder_count) ? $purchaseorder_count : 0 }}</span>
         </div>
         <!-- /.info-box-content -->
       </div>
@@ -35,8 +35,8 @@
         <span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>
 
         <div class="info-box-content">
-          <span class="info-box-text">Likes</span>
-          <span class="info-box-number">41,410</span>
+          <span class="info-box-text">Receipt</span>
+          <span class="info-box-number">{{ isset($receipt_count) ? $receipt_count : 0 }}</span>
         </div>
         <!-- /.info-box-content -->
       </div>
@@ -52,8 +52,8 @@
         <span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
 
         <div class="info-box-content">
-          <span class="info-box-text">Sales</span>
-          <span class="info-box-number">760</span>
+          <span class="info-box-text">Supply Inventory</span>
+          <span class="info-box-number">{{ isset($supply_count) ? $supply_count : 0 }}</span>
         </div>
         <!-- /.info-box-content -->
       </div>
@@ -65,8 +65,8 @@
         <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
 
         <div class="info-box-content">
-          <span class="info-box-text">New Members</span>
-          <span class="info-box-number">2,000</span>
+          <span class="info-box-text">Requisition and Issue Slip</span>
+          <span class="info-box-number">{{ isset($ris_count) ? $ris_count : 0 }}</span>
         </div>
         <!-- /.info-box-content -->
       </div>
@@ -80,7 +80,7 @@
     <div class="col-md-12">
       <div class="box">
         <div class="box-header with-border">
-          <h3 class="box-title">Monthly Recap Report</h3>
+          <h3 class="box-title">Monthly Supply Release Report</h3>
 
           <div class="box-tools pull-right">
             <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
