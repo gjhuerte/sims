5e9b2aeb4cccc959c9cf4ececbb85373f6a01826
Unknown <kaiser_FR3D@yahoo.com.ph>
2018-01-15 15:40:33 +0800

    Updated RIS printable
    incompliance with the new RIS form

--- resources/views/request/print_show.blade.php
+++ resources/views/request/print_show.blade.php
@@ -10,12 +10,16 @@
     <table class="table table-striped table-bordered" id="inventoryTable" width="100%" cellspacing="0">
       <thead>
           <tr rowspan="2">
-              <th class="text-left" colspan="3">Request Slip:  <span style="font-weight:normal">{{ $request->code }}</span> </th>
-              <th class="text-left" colspan="3">Requestor:  <span style="font-weight:normal">{{ isset($request->office->name) ? $request->office->name : $request->office }}</span> </th>
+              <th class="text-left" colspan="3"></th>
+              <th class="text-left" colspan="3">Fund Cluster:  <span style="font-weight:normal"></span> </th>
           </tr>
           <tr rowspan="2">
-              <th class="text-left" colspan="3">Date Requested:  <span style="font-weight:normal">{{ Carbon\Carbon::parse($request->created_at)->toFormattedDateString() }}</span> </th>
-              <th class="text-left" colspan="3">Status:  <span style="font-weight:normal">{{ isset($request->status) ? $request->status : ucfirst(config('app.default_status')) }}</span> </th>
+              <th class="text-left" colspan="3">Division:  <span style="font-weight:normal"><u>{{ isset($request->office->name) ? $request->office->name : $request->office }}</span> </u></th>
+              <th class="text-left" colspan="3">Responsibility Center Code:  <span style="font-weight:normal"></span> </th>
+          </tr>
+          <tr rowspan="2">
+              <th class="text-left" colspan="3">Office: </th>
+              <th class="text-left" colspan="3">Status:  <span style="font-weight:normal">{{ $request->code }}</span> </th>
           </tr>
           <tr rowspan="2">
               <th class="text-left" colspan="6">Remarks:  <span style="font-weight:normal">{{ $request->remarks }}</span> </th>
@@ -43,10 +47,21 @@
       </tbody>
     </table>
   </div>
+  <div id="content" class="col-sm-12">
+    <table class="table table-striped table-bordered" id="inventoryTable" width="100%" cellspacing="0">
+      <thead>
+          <tr rowspan="2">
+              <th class="text-left" colspan="3">Purpose:_______________________________________________________________________________________________
+                                            <br>_______________________________________________________________________________________________________</th>
+          </tr>
+      </thead>
+    </table>
+  </div>
   <div id="footer" class="col-sm-12">
     <table class="table table-bordered">
       <thead>
         <tr>
+          <th class="col-sm-1">   </th>
           <th class="col-sm-1">  Requested By: </th>
           <th class="col-sm-1">  Approved By: </th>
           <th class="col-sm-1">  Issued By: </th>
@@ -56,37 +71,52 @@
         </tr>
       </thead>
       <tbody>
+          <td class="text-center">
+            Signature:
+          </td>
+          <td class="text-center">
+          </td>
+          <td class="text-center">
+          </td>
+          <td class="text-center">
+          </td>
         <tr>
           <td class="text-center">
-            <br />
-            <br />
+            Printed Name:
+          </td>
+          <td class="text-center">
             <span id="name" style="margin-top: 30px; font-size: 15px;"> {{ (count($request->requestor) > 0) ? $request->requestor->firstname . " " . $request->requestor->lastname : "" }}</span>
             <br />
-            <span id="office" class="text-center" style="font-size:10px;">{{ isset($request->office) ? $request->office->name : "" }}</span>
           </td>
           <td class="text-center">
-            <br />
-            <br />
             <span id="name" style="margin-top: 30px; font-size: 15px;"> {{ isset($approvedby->head) ? $approvedby->head : "" }}</span>
             <br />
-            <span id="office" class="text-center" style="font-size:10px;">{{ isset($approvedby->name) ? $approvedby->name : "" }}</span>
           </td>
           <td class="text-center">
-            <br />
-            <br />
             <span id="name" style="margin-top: 30px; font-size: 15px;"></span>
             <br />
-            <span id="office" class="text-center" style="font-size:10px;"></span>
           </td>
           <td class="text-center">
-            <br />
-            <br />
             <span id="name" style="margin-top: 30px; font-size: 15px;"> </span>
             <br />
             <span id="office" class="text-center" style="font-size:10px;"></span>
           </td>
           {{-- <td></td>
           <td></td> --}}
+          </tr>
+          <tr>
+          <td class="text-center">
+            Designation:
+          </td>
+          <td class="text-center">
+            <span id="office" class="text-center" style="font-size:10px;">{{user->position}},{{ isset($request->office) ? $request->office->code : "" }}</span>
+          </td>
+          <td class="text-center">
+            <span id="office" class="text-center" style="font-size:10px;">,{{ isset($approvedby->name) ? $approvedby->code : "" }}</span>
+          </td>
+          <td class="text-center">
+            <span id="office" class="text-center" style="font-size:10px;"></span>
+          </td>
         </tr>
       </tbody>
     </table>
