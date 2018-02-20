0a721745bb3b9695d78603bebc689eedcc74513b
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-02-20 17:54:59 +0800

    Updated Footer for Physical Inventory Report

--- app/StockCard.php
+++ app/StockCard.php
@@ -589,4 +589,12 @@ class StockCard extends Model implements Auditable, UserResolver
 			$stockcard->save();
 		}
 	}
+
+	/**
+	 * save the rows for inspection
+	 */
+	public function forInspection()
+	{
+
+	}
 }
