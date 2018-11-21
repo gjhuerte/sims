c436b49b599576ea6d8b9d22acb3f8a9ca225937
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-11-21 15:21:14 +0800

    Category model refactoring

---
+++ app/Http/Requests/CategoryRequest/CategoryUpdateRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Requests\CategoryRequest;
+
+use App\Models\Supply\Category;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoryUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        if(Auth::check()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $category = Category::findOrFail($this->category);
+        return [
+			'code' => 'required|max:20|unique:categories,uacs_code,'.$category->code.',uacs_code',
+			'name' => 'required|max:200|unique:categories,name,'.$category->name.',name'
+        ];
+    }
+
+    /**
+     * Appends the parameters of the route to the
+     * to the list of request the object has and merge
+     * it into the parameters
+     *
+     * @return this
+     */
+    public function validationData()
+    {
+        if (method_exists($this->route(), 'parameters')) {
+            $this->request->add($this->route()->parameters('id'));
+            $this->query->add($this->route()->parameters('id'));
+
+            return array_merge($this->route()->parameters(), $this->all());
+        }
+
+        return $this->all();
+    }
+}
